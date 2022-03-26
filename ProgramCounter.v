2e9a68c3a3bf75e4399e3cb0e1c926efda051757
seankwon7 <seankwon7@gmail.com>
2022-03-26 22:46:20 +0900

    PC fixed

--- ProgramCounter.v
+++ ProgramCounter.v
@@ -3,8 +3,6 @@ module PC ( input reset,
             input [31:0] next_pc, 
             output reg current_pc);
   
-  current_pc <= 32'b0;
-  
   always@(posedge clk) begin
     if(reset) current_pc <= 32'b0;
     else current_pc <= next_pc;
