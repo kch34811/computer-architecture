a6fd80197f5bd019b33bc8ad794f7212b59ad010
seankwon7 <seankwon7@gmail.com>
2022-03-26 20:45:28 +0900

    immediateGenerator fixed

--- ImmediateGenerator.v
+++ ImmediateGenerator.v
@@ -15,16 +15,16 @@ module ImmediateGenerator(input [6:0] part_of_inst, input [31:0] all_of_inst, ou
     `JALR : imm_gen_out = {all_of_inst[31]? 20'b1 : 20'b0, all_of_inst[30:20]};
 
     // S-type instruction
-    `STORE : 
+    `STORE : imm_gen_out = {all_of_inst[31]? 20'b1 : 20'b0, all_of_inst[31:25], all_of_inst[11:7]};
 
     // B-type instruction
-    `BRANCH :
+    `BRANCH : imm_gen_out = {all_of_inst[31]? 20'b1:20'b0, all_of_inst[7], all_of_inst[30:25], all_of_inst[11:8], 1'b0};
 
     // U-type instruction
-    `LUI :
-    `AUIPC :
+    `LUI : imm_gen_out = {all_of_inst[31:12], 12'b0};
+    `AUIPC : imm_gen_out = {all_of_inst[31:12], 12'b0};
 
     // J-type instruction
-    `JAL :
+    `JAL : imm_gen_out = {all_of_inst[31]? 20'b1:20'b0, all_of_inst[19:12], all_of_inst[19:12], all_of_inst[20], all_of_inst[30:25], all_of_inst[24:21], 1'b0}
 
 endmodule
