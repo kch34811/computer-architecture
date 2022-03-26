984bc3c11b589d58984e0651f735c3eb6aed3160
seankwon7 <seankwon7@gmail.com>
2022-03-26 23:59:25 +0900

    Imm_gen fixed

--- ImmediateGenerator.v
+++ ImmediateGenerator.v
@@ -24,8 +24,8 @@ module ImmediateGenerator(input [6:0] part_of_inst, input [31:0] all_of_inst, ou
     `BRANCH : imm_gen_out = {all_of_inst[31]? 20'b1:20'b0, all_of_inst[7], all_of_inst[30:25], all_of_inst[11:8], 1'b0};
 
     // U-type instruction
-    `LUI : imm_gen_out = {all_of_inst[31:12], 12'b0};
-    `AUIPC : imm_gen_out = {all_of_inst[31:12], 12'b0};
+    //`LUI : imm_gen_out = {all_of_inst[31:12], 12'b0};
+    //`AUIPC : imm_gen_out = {all_of_inst[31:12], 12'b0};
 
     // J-type instruction
     `JAL : imm_gen_out = {all_of_inst[31]? 20'b1:20'b0, all_of_inst[19:12], all_of_inst[19:12], all_of_inst[20], all_of_inst[30:25], all_of_inst[24:21], 1'b0}
