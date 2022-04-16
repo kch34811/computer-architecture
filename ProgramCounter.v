d63c6a18fe49af3d2ebfbc79ffa1ebb0d826e5b3
kch34811 <kch34811@gmail.com>
2022-04-17 01:01:45 +0900

    Make

---
+++ ProgramCounter.v
@@ -0,0 +1,15 @@
+module PC ( input reset, 
+            input clk, 
+            input PC_control,
+            input [31:0] next_pc, 
+            output reg [31:0] current_pc);
+  
+  always@(posedge clk) begin
+    if(reset) begin
+      current_pc <= 32'b0;
+    end
+    else begin
+      if (PC_control) current_pc <= next_pc;
+    end
+  end
+endmodule
--- cpu.v
+++ cpu.v
@@ -12,6 +12,43 @@ module CPU(input reset,       // positive reset signal
            input clk,         // clock signal
            output is_halted); // Whehther to finish simulation
   /***** Wire declarations *****/
+  wire [31:0] PCIn;
+  wire [31:0] PCOut;
+  wire [31:0] WriteData;
+  wire [31:0] MemData;
+  wire [31:0] rs1_dout;
+  wire [31:0] rs2_dout;
+  wire [31:0] ALUResult;
+  wire [31:0] ImmGenOut;
+
+  wire [31:0] MUX1Out;
+  wire [31:0] MUX2Out;
+  wire [31:0] MUX3Out;
+  wire [31:0] MUX4Out;
+  wire [31:0] MUX5Out;
+
+  wire PCWriteNotCond;
+  wire PCWrite;
+  wire lorD;
+  wire MemRead;
+  wire MemWrite;
+  wire MemtoReg;
+  wire IRWrite;
+  wire PCSource;
+  wire [3:0] ALUop;
+  wire ALUSrcB;
+  wire ALUSrcA;
+  wire RegWrite;
+  wire bcond;
+
+  wire IR_wire;
+  wire MDR_wire;
+  wire A_wire;
+  wire B_wire;
+  wire ALUOut_wire;
+
+  wire isEcall;
+  wire [31:0] x17;
 
   /***** Register declarations *****/
   reg [31:0] IR; // instruction register
@@ -21,42 +58,49 @@ module CPU(input reset,       // positive reset signal
   reg [31:0] ALUOut; // ALU output register
   // Do not modify and use registers declared above.
 
+  assign IR_wire = IR;
+  assign MDR_wire = MDR;
+  wire A_wire = A;
+  wire B_wire = B;
+  wire ALUOut_wire = ALUOut;
+
   // ---------- Update program counter ----------
   // PC must be updated on the rising edge (positive edge) of the clock.
   PC pc(
-    .reset(),       // input (Use reset to initialize PC. Initial value must be 0)
-    .clk(),         // input
-    .next_pc(),     // input
-    .current_pc()   // output
+    .reset(reset),       // input (Use reset to initialize PC. Initial value must be 0)
+    .clk(clk),         // input
+    .PC_control((~becond & PCWriteNotCond) | PCWrite), // input
+    .next_pc(PCIn),     // input
+    .current_pc(PCOut)   // output
   );
 
   // ---------- Register File ----------
   RegisterFile reg_file(
-    .reset(),        // input
-    .clk(),          // input
-    .rs1(),          // input
-    .rs2(),          // input
-    .rd(),           // input
-    .rd_din(),       // input
-    .write_enable(),    // input
-    .rs1_dout(),     // output
-    .rs2_dout()      // output
+    .reset(reset),        // input
+    .clk(clk),          // input
+    .rs1(IR_wire[19:15]),          // input
+    .rs2(IR_wire[24:20]),          // input
+    .rd(IR_wire[11:7]),           // input
+    .rd_din(MUX2Out),       // input
+    .write_enable(RegWrite),    // input
+    .rs1_dout(rs1_dout),     // output
+    .rs2_dout(rs2_dout)      // output
   );
 
   // ---------- Memory ----------
   Memory memory(
-    .reset(),        // input
-    .clk(),          // input
-    .addr(),         // input
-    .din(),          // input
-    .mem_read(),     // input
-    .mem_write(),    // input
-    .dout()          // output
+    .reset(reset),        // input
+    .clk(clk),          // input
+    .addr(MUX1Out),         // input
+    .din(B_wire),          // input
+    .mem_read(MemRead),     // input
+    .mem_write(MemWrite),    // input
+    .dout(MemData)          // output
   );
 
   // ---------- Control Unit ----------
   ControlUnit ctrl_unit(
-    .part_of_inst(),  // input
+    .part_of_inst(IR_wire[6:0]),  // input
     .is_jal(),        // output
     .is_jalr(),       // output
     .branch(),        // output
@@ -71,23 +115,45 @@ module CPU(input reset,       // positive reset signal
 
   // ---------- Immediate Generator ----------
   ImmediateGenerator imm_gen(
-    .part_of_inst(),  // input
-    .imm_gen_out()    // output
+    .part_of_inst(IR_wire[31:0]),  // input
+    .imm_gen_out(ImmGenOut)    // output
   );
 
   // ---------- ALU Control Unit ----------
   ALUControlUnit alu_ctrl_unit(
-    .part_of_inst(),  // input
-    .alu_op()         // output
+    .part_of_inst(IR_wire[31:0]),  // input
+    .alu_op(ALUop)         // output
   );
 
   // ---------- ALU ----------
   ALU alu(
-    .alu_op(),      // input
-    .alu_in_1(),    // input  
-    .alu_in_2(),    // input
-    .alu_result(),  // output
-    .alu_bcond()     // output
+    .alu_op(ALUop),      // input
+    .alu_in_1(MUX3Out),    // input  
+    .alu_in_2(MUX4Out),    // input
+    .alu_result(ALUResult),  // output
+    .alu_bcond(bcond)     // output
   );
 
+  MUX2_to_1 #(32) MUX1 (PCOut, ALUOut_wire, lorD, MUX1Out);
+  MUX2_to_1 #(32) MUX2 (ALUOut_wire, MDR_wire, MemtoReg, MUX2Out);
+  MUX2_to_1 #(32) MUX3 (PCOut, A_wire, ALUSrcA, MUX3Out);
+  MUX4_to_1 #(32) MUX4 (B_wire, 4, ImmGenOut, 0, ALUSrcB, MUX4Out);
+  MUX2_to_1 #(32) MUX5 (ALUResult, ALUOut_wire, PCSource, MUX5Out);
+
+  always @(posedge clk) begin
+    if (reset) begin
+      IR <= 0;
+      MDR <= 0;
+      A <= 0;
+      B <= 0;
+      ALUOut <= 0;
+    end
+    else begin
+      if(IRWrite) IR <= MemData;
+      MDR <= MemData;
+      A <= rs1_dout;
+      B <= rs2_dout;
+      ALUOut <= ALUResult;
+    end
+  end
 endmodule
