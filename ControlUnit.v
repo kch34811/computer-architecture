705957d16a89d6484350727a6d0764f98e55fca4
kch34811 <kch34811@gmail.com>
2022-04-18 01:29:16 +0900

    Cont fixed

--- ControlUnit.v
+++ ControlUnit.v
@@ -40,7 +40,12 @@ module ControlUnit (input [6:0] part_of_inst,
             state <= `INST_FETCH;
         end else begin
             if (state == `INST_FETCH) begin
-                    state <= `INST_DECODE_REG_FETCH;
+                if (MemData == `JAL) begin
+                    state <= `JUMP_IMM;
+                end
+                else begin
+                state <= `INST_DECODE_REG_FETCH;
+                end
             end else if (state == `INST_DECODE_REG_FETCH) begin
                 if (part_of_inst == `LOAD || part_of_inst == `STORE) begin
                     state <= `MEM_ADDR_COMPUTATION;
@@ -52,9 +57,10 @@ module ControlUnit (input [6:0] part_of_inst,
                     state <= `HALT;
                 end else if (part_of_inst == `ARITHMETIC_IMM || part_of_inst == `JALR) begin
                     state <= `EXECUTION_IMM;
-                end else if (part_of_inst == `JAL) begin
-                    state <= `JUMP_IMM;
-                end
+                end 
+                //else if (part_of_inst == `JAL) begin
+                    //state <= `JUMP_IMM;
+                //end
             end else if(state == `MEM_ADDR_COMPUTATION) begin
                 if(part_of_inst == `LOAD) begin
                     state <= `MEM_ACCESS_READ;
@@ -99,55 +105,46 @@ module ControlUnit (input [6:0] part_of_inst,
         is_ecall = 1'b0;
 
         if (state == `INST_FETCH) begin
-            mem_read = 1'b1;
-            ALU_src_b = 2'b01;
-            if(MemData ==`JAL) begin
-                PC_write = 1'b0;
-                reg_write = 1'b1;
-                $display("jal");
-            end else begin
-                PC_write = 1'b1;
-                $display("jal !");
-            end
-            IR_write = 1'b1;
+            mem_read <= 1'b1;
+            ALU_src_b <= 2'b01;
+            IR_write <= 1'b1;
         end else if (state == `INST_DECODE_REG_FETCH) begin
-            ALU_src_b = 2'b10;
-            //if(part_of_inst == `JAL) begin
-                //reg_write = 1'b1;
-            //end
+            ALU_src_b <= 2'b10;
+            PC_write = 1'b1;
+            PC_source = 1'b1; 
         end else if (state == `MEM_ADDR_COMPUTATION) begin
-            ALU_src_a = 1'b1;
-            ALU_src_b = 2'b10;
+            ALU_src_a <= 1'b1;
+            ALU_src_b <= 2'b10;
         end else if (state == `MEM_ACCESS_READ) begin
-            mem_read = 1'b1;
-            i_or_d = 1'b1;
+            mem_read <= 1'b1;
+            i_or_d <= 1'b1;
         end else if (state == `WB_STEP) begin
-            reg_write = 1'b1;
-            mem_to_reg = 1'b1;
+            reg_write <= 1'b1;
+            mem_to_reg <= 1'b1;
         end else if (state == `MEM_ACCESS_WRITE) begin
-            mem_write = 1'b1;
-            i_or_d = 1'b1;
+            mem_write <= 1'b1;
+            i_or_d <= 1'b1;
         end else if (state == `EXECUTION) begin
-            ALU_src_a = 1'b1;
-            ALU_src_b = 2'b00;
-            ALU_op = 2'b10;
+            ALU_src_a <= 1'b1;
+            ALU_src_b <= 2'b00;
+            ALU_op <= 2'b10;
         end else if (state == `R_TYPE_COMPLETION) begin
-            reg_write = 1'b1;
+            reg_write <= 1'b1;
         end else if (state == `BRANCH_COMPLETION) begin
-            ALU_src_a = 1'b1;
-            ALU_src_b = 2'b00;
-            ALU_op = 2'b01;
-            PC_write_cond = 1'b1;
-            PC_source = 1'b1;
+            ALU_src_a <= 1'b1;
+            ALU_src_b <= 2'b00;
+            ALU_op <= 2'b01;
+            PC_write_cond <= 1'b1;
+            PC_source <= 1'b1;
         end else if (state == `HALT) begin
-            is_ecall = 1'b1;
+            is_ecall <= 1'b1;
         end else if (state == `EXECUTION_IMM) begin
-            ALU_src_a = 1'b1;
-            ALU_src_b = 2'b10;
-            ALU_op = 2'b10;
+            ALU_src_a <= 1'b1;
+            ALU_src_b <= 2'b10;
+            ALU_op <= 2'b10;
         end else if (state == `JUMP_IMM) begin
-            ALU_src_b = 2'b01;
-            PC_source = 1'b1;
+            ALU_src_b <= 2'b10;
+            PC_write <= 1'b1;
         end
     end
 
