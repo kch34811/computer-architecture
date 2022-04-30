9e56f794e370c05fc3a5467d4efd7b8b57f5b011
kch34811 <kch34811@gmail.com>
2022-05-01 03:37:38 +0900

    PC

--- PC.v
+++ PC.v
@@ -1,11 +1,14 @@
 module PC ( input reset, 
             input clk, 
+            input PC_control,
             input [31:0] next_pc, 
             output reg [31:0] current_pc);
   
   always@(posedge clk) begin
     if(reset) current_pc <= 32'b0;
-    else current_pc <= next_pc;
+    else begin
+      if(PC_control) current_pc <= next_pc;
+    end
   end
 
 endmodule
