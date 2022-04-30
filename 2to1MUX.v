f6d8dfad299905ca561b06a5dd7f170e017f4c72
kch34811 <kch34811@gmail.com>
2022-05-01 00:53:18 +0900

    Mux Fixed

--- 2to1MUX.v
+++ 2to1MUX.v
@@ -6,3 +6,12 @@ module MUX2_to_1 (input [31:0] A,
 	assign Q = a ? B : A;
 
 endmodule
+
+module Ecall_MUX (input [4:0] A, 
+									input [4:0] B, 
+									input a, 
+									output [4:0] Q);
+
+	assign Q = a ? B : A;
+
+endmodule
--- EcallMUX.v
+++
@@ -1,8 +0,0 @@
-module Ecall_MUX (input [4:0] A, 
-									input [4:0] B, 
-									input a, 
-									output [4:0] Q);
-
-	assign Q = a ? B : A;
-
-endmodule
