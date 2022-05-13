903962e1f9a219d71069489668deabdd35ecaf26
kch34811 <kch34811@gmail.com>
2022-05-13 23:26:18 +0900

    Make branch predictor

--- branch_predictor.v
+++ branch_predictor.v
@@ -3,8 +3,8 @@ module branch_predictor(input reset,
                         input is_branch,
                         input [31:0] current_pc, 
                         input [31:0] next_pc,
-                        output reg [31:0] current_pc);
+                        );
 
   
-  
+
 endmodule
